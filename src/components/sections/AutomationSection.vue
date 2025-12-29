ccf527022fa7b91f9b08898490ab87d2e03b8513
jiyi-stack <1721369356@qq.com>
2025-12-29 16:17:20 +0800

    自动化脚本步骤完成

--- src/components/sections/AutomationSection.vue
+++ src/components/sections/AutomationSection.vue
@@ -64,7 +64,8 @@
           <h3>✅ 登录及首页介绍</h3>
           <ul>
             <li>
-              1. 打开应用后，使用企业微信扫码登录，登录成功后即可进入首页
+              1.
+              以V0.5.0为例，打开晓阳AI助手后，使用企业微信扫码登录，登录成功后进入首页
               <div>
                 <img
                   style="max-height: 50vh; margin: 16px 6px"
@@ -78,7 +79,11 @@
               登录成功后跳转首页，会显示本地已有的脚本，可以根据名字模糊匹配进行搜索🔍
               <br />
               &nbsp;&nbsp;&nbsp;运行脚本即可执行自动化任务，脚本卡片右上角有当前运行状态，例如：<span
-                style="color: #5366a6; font-weight: 700"
+                style="
+                  color: #5366a6;
+                  font-weight: 700;
+                  text-decoration: underline;
+                "
                 >运行中、成功、失败</span
               >
               等
@@ -112,38 +117,49 @@
             <li>
               1. 点击下方第二个菜单图标，进入新建页面，支持两种方式：
               <span><img src="@/assets/record.svg" alt="" /> 录制脚本</span
-              >和手动<span><img src="@/assets/add.svg" alt="" /> 添加脚本</span>
+              >和<span
+                ><img src="@/assets/add.svg" alt="" /> 手动点击添加步骤</span
+              >
             </li>
             <li>
-              2.
-              可以直接点击录制按钮，屏幕右下角会出现三个按钮，从左到右分别是停止、录制、返回按钮
+              2. 顶部输入框输入脚本流程名称，未输入将默认脚本名称， 请知悉。
               <br />
               <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
+                <img src="@/assets/aoto-tittle.png" alt="" />
               </div>
             </li>
             <li>
-              3. 点击录制按钮就可以开始录制屏幕的操作了
+              3.
+              点击录制按钮后，屏幕右下角会录制工作区，这三个按钮，从左到右分别是停止、录制、返回按钮
+            </li>
+            <li>
+              4.
+              点击录制按钮后开始自动记录操作步骤，等待识别完成将录下下一个步骤
               <div class="tip-box">
                 <span class="tip-icon">提示：</span>
                 <span class="tip-content">
                   录制过程中，需要操作慢一点，当左下角显示"识别中..."时，请不要操作，否则步骤可能会丢失</span
                 >
               </div>
-              <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
+              <br />
+              <div>
+                <img
+                  src="@/assets/auto-start.gif"
+                  alt=""
+                  style="width: 280px; margin: 16px 0 0 0"
+                />
               </div>
             </li>
             <li>
-              4.录制完成后点击停止录制，再次点击返回，可以看见页面上出现刚刚录制的步骤
+              5.点击停止录制，再次点击返回，可以看见返回刚刚已录入的脚本详情
               <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
+                <img src="@/assets/auto-detaile2.png" alt="" />
               </div>
             </li>
             <li>
-              5.点击执行程序可以自动执行刚刚手动操作的步骤
+              6.点击执行程序可以自动执行刚刚手动操作的步骤，执行完成会自动返回首页
               <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
+                <img src="@/assets/auto-run.gif" alt="" />
               </div>
             </li>
           </ul>
@@ -154,17 +170,17 @@
           <h3>✅ 如何手动添加脚本</h3>
           <ul>
             <li>
-              1. 点击下方添加步骤按钮，在弹出的插件指令中选择指令
+              1. 点击下方添加步骤按钮，在弹出的插件指令中选择对应指令
               <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
+                <img src="@/assets/auto-addstep.png" alt="" />
               </div>
             </li>
             <li>
               2.
-              将鼠标悬浮到已添加的步骤上时，会自动悬浮显示添加按钮，点击后即可在该步骤后添加新步骤
+              将鼠标悬浮到已添加的步骤上时，会自动悬浮显示添加按钮，点击后即可在插入在该步骤后面，添加新步骤再执行
               <br />
               <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
+                <img src="@/assets/auto-add.gif" alt="" />
               </div>
             </li>
           </ul>
@@ -178,59 +194,49 @@
               1.
               首先进入首页，点击想要编辑的脚本，进入编辑页面，这里支持修改脚本名称，新增、修改、删除、移动步骤
               <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
+                <img src="@/assets/auto-edit.gif" alt="" />
               </div>
             </li>
             <li>
-              2.
+              ·
               新增：与手动添加步骤类似，悬浮到已有步骤上，点击新增动画图标，可以添加新的步骤
               <br />
-              <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
-              </div>
             </li>
             <li>
-              3. 修改：悬浮到已有步骤上，点击修改图标，可以修改该步骤
+              · 修改：悬浮到已有步骤上，点击修改图标，可以修改该步骤
               <br />
-              <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
-              </div>
             </li>
             <li>
-              4. 删除：悬浮到已有步骤上，点击删除图标，可以删除该步骤
+              · 删除：悬浮到已有步骤上，点击删除图标，可以删除该步骤
               <br />
-              <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
-              </div>
             </li>
             <li>
-              5.
+              ·
               移动：悬浮到已有步骤名称上，点击可以拖动该步骤到指定位置，其余步骤顺序顺移
               <br />
+            </li>
+            <li>
+              2.当前脚本详情页里，也支持录制更新、执行和保存脚本等信息
+              <br />
               <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
+                <img
+                  src="@/assets/auto-menu.png"
+                  alt=""
+                  style="width: 350px; margin: 16px 0 0 0"
+                />
               </div>
             </li>
             <li>
-              6. 录制：点击后，可以在弹出框中，选择覆盖或追加步骤
+              · 执行：上方蓝色执行按钮表示执行该脚本全部内容
               <br />
-              <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
-              </div>
             </li>
             <li>
-              7. 执行：上方蓝色执行按钮表示执行该脚本全部内容
+              · 录制：点击后，可以在弹出框中，选择覆盖或追加步骤
               <br />
-              <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
-              </div>
             </li>
             <li>
-              8. 测试：点击后单步执行当前步骤
+              · 保存：点击保存按钮，即可保存当前已编辑的脚本
               <br />
-              <div class="imgStyle">
-                <img src="@/assets/img.png" alt="" />
-              </div>
             </li>
           </ul>
         </div>
@@ -473,12 +479,12 @@ export default {
 }
 
 .imgStyle {
-  margin: 16px 6px;
-  width: 80px;
+  margin: 16px 0 0 0;
+  width: 70px;
 }
 
-/* .imgStyle img {
-  width: 260%;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
-} */
+.imgStyle img {
+  width: 380%;
+  /* box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1); */
+}
 </style>
